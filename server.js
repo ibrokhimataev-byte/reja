139e8c51cbf4d970f4e150a605d01ea0971ba642
Leo <ibrokhimataev@gmail.com>
2025-12-25 11:54:11 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,42 @@
+console.log("web serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+
+// 1 - Krish code: Bosqichda expressga krib kelayotgan malunotlarga bogliq bolgan kodlarmz nomi.
+
+app.use(express.static("public"));                                  // har qanday brauzerdan krib kelayotgan zaproslar uchun public folder ochiq, faqat public folder koroladi degan manoni anglatadi.
+app.use(express.json());                                           // json formatdagi datalarni object xolatiga bizlarga ogrib beradi.
+app.use(express.urlencoded( {extended: true}));                   //htmldan traditional request forum shakildan express qabul qlib oladi agar codni yozmasak ignore qiladi.
+
+
+
+
+
+// 2 sessionga bogliq express codelari
+
+
+
+// 3 views code
+app.set("views", "views");
+app.set("view engine", "ejs"); // htnml frontendni yasaymiz backandni ichida
+
+
+// 4 Routing code
+app.get("/", function(req, res){
+    res.end("<h1>HELLO WORLD</h1>");
+
+});
+app.get("/gift", function(req, res){
+    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+
+});
+
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
