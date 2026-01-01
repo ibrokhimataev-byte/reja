a5a25bf33a092768eccf5d1682f28cb2ce361e3d
Leo <ibrokhimataev@gmail.com>
2026-01-01 17:44:37 +0900

    feat: MongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,59 @@
+console.log("web serverni boshlash");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+// const fs = require("fs");
+
+// let user;
+// fs.readFile("database/users.json", "utf-8",(err, data)=>{
+//     if(err){
+//         console.log("Error", err);
+//     }   else{
+//         user = JSON.parse(data);
+// }       
+// });
+
+
+
+// MongoDB chaqirish
+const db = require("./server").db();
+
+
+// 1 - Krish code: Bosqichda expressga krib kelayotgan malunotlarga bogliq bolgan kodlarmz nomi.
+
+app.use(express.static("public"));                                  // har qanday brauzerdan krib kelayotgan zaproslar uchun public folder ochiq, faqat public folder koroladi degan manoni anglatadi.
+app.use(express.json());                                           // json formatdagi datalarni object xolatiga bizlarga ogrib beradi.
+app.use(express.urlencoded( {extended: true}));                   //htmldan traditional request forum shakildan express qabul qlib oladi agar codni yozmasak ignore qiladi.
+
+
+
+
+
+// 2 sessionga bogliq express codelari
+
+
+
+// 3 views code
+app.set("views", "views");
+app.set("view engine", "ejs"); // htnml frontendni yasaymiz backandni ichida
+
+
+// 4 Routing code
+app.post("/create-item", function(req, res) {
+    console.log(req.body);
+    res.json( {test: "success"} );
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user} );
+});
+
+app.get("/", function(req, res){
+    res.render("reja");
+});
+
+
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,57 +1,24 @@
-console.log("web serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/users.json", "utf-8",(err, data)=>{
-    if(err){
-        console.log("Error", err);
-    }   else{
-        user = JSON.parse(data);
-}       
-});
+let db;
+const connectionString = "mongodb+srv://alberto010126:WFr3EZXKvBvdPdRH@cluster0.2jee3my.mongodb.net/Reja?appName=Cluster0";
 
-// 1 - Krish code: Bosqichda expressga krib kelayotgan malunotlarga bogliq bolgan kodlarmz nomi.
-
-app.use(express.static("public"));                                  // har qanday brauzerdan krib kelayotgan zaproslar uchun public folder ochiq, faqat public folder koroladi degan manoni anglatadi.
-app.use(express.json());                                           // json formatdagi datalarni object xolatiga bizlarga ogrib beradi.
-app.use(express.urlencoded( {extended: true}));                   //htmldan traditional request forum shakildan express qabul qlib oladi agar codni yozmasak ignore qiladi.
-
-
-
-
-
-// 2 sessionga bogliq express codelari
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true,},
+    (err, client) => {
+        if(err) console.log("ERROR on connection MongoDB");
+        else{
+            console.log("MongoDB connection succeed");
+            module.exports = client;
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function(){
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+});        
+        }
+    }
+);
 
 
 
-// 3 views code
-app.set("views", "views");
-app.set("view engine", "ejs"); // htnml frontendni yasaymiz backandni ichida
-
-
-// 4 Routing code
-app.post("/create-item", function(req, res) {
-    console.log(req.body);
-    res.json( {test: "success"} );
-});
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user} );
-});
-
-app.get("/", function(req, res){
-    res.render("reja");
-});
-
-
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
